8782938cbd21f34f097ffd32bec0cafc215f687d
Matheus Silva <matpaixaoo@outlook.com>
2024-08-10 12:03:42 -0300

    feat: dois exercícios (revisão clang)

--- ED_100824/main.c
+++ ED_100824/main.c
@@ -10,9 +10,9 @@ int recebeValores() {
 	system("cls");
 	
 	do {
+		fflush(stdin);
 		printf("Digite o %d° número: ", i + 1);
 		scanf("%d", &v[i]);
-		fflush(stdin);
 		i++;
 	} while(i < LEN);
 	
@@ -39,13 +39,108 @@ int recebeValores() {
 	}
 }
 
+int inverteVetor() {
+	
+	int v[LEN], aux1, aux2;
+	int i = 0, j;
+	system("cls");
+	
+	do {
+		printf("Digite o %d° número: ", i + 1);
+		scanf("%d", &v[i]);
+		fflush(stdin);
+		i++;
+	} while(i < LEN);
+	
+	for (i = 0; i < LEN/2; i++){
+		aux1 = v[i];
+		aux2 = v[LEN - (i + 1)];
+		
+		v[i] = aux2;
+		v[LEN - (i + 1)] = aux1;
+	}
+	
+	for (i = 0; i < LEN; i++) {
+		printf("%d ", v[i]);
+	}
+	
+	char c;
+	
+	fflush(stdin);
+	printf("\n\nGostaria de refazer? (y/n): ");
+	scanf("%c", &c);
+	
+	switch(c) {
+		case 'y':
+			return 1;
+			break;
+		case 'n':
+			return 0;
+			break;
+		default:
+			return 0;
+			break;
+	}
+}
+
+int maiorMenor() {
+	
+	int v[LEN], maior, menor;
+	int i = 0;
+	
+	system("cls");
+	
+	do {
+		printf("Digite o %d° número: ", i + 1);
+		scanf("%d", &v[i]);
+		fflush(stdin);
+		i++;
+	} while(i < LEN);
+	
+	maior = v[0];
+	menor = v[0];
+	
+	for(i = 0; i < LEN; i++){
+		if(v[i] > maior){
+			maior = v[i];
+		}
+		
+		if(v[i] < menor){
+			menor = v[i];
+		}
+	}
+	
+	printf("O menor valor é: %d\n", menor);
+	printf("O maior valor é: %d\n", maior);
+	
+	char c;
+	
+	fflush(stdin);
+	printf("\n\nGostaria de refazer? (y/n): ");
+	scanf("%c", &c);
+	
+	switch(c) {
+		case 'y':
+			return 1;
+			break;
+		case 'n':
+			return 0;
+			break;
+		default:
+			return 0;
+			break;
+	}
+}
+
 int main() {
 	
 	setlocale(LC_ALL, "portuguese");
 	int b;
-	
+
 	do{
-		b = recebeValores();
+//		b = recebeValores();
+//		b = inverteVetor();
+		b = maiorMenor();
 	}while(b == 1);
 
 	return 0;
